f8b6201943d91ada48dc5ff86db1a83088ff6c09
m2a <yewkheng.kwok@my.jcu.edu.au>
2023-11-27 15:14:06 +0800

    Modified Prac 4

--- prac_04/list_exercise.py
+++
@@ -1,9 +0,0 @@
-usernames = ['jimbo', 'giltson98', 'derekf', 'WhatSup', 'NicolEye', 'swei45', 'BaseInterpreterInterface', 'BaseStdIn',
-             'Command', 'ExecState', 'InteractiveConsole', 'InterpreterInterface', 'StartServer', 'bob']
-
-try_username = input("Whats your username? ")
-
-if try_username in usernames:
-    print("Access Granted")
-else:
-    print("Access Denied")
--- prac_04/list_exercises.py
+++ prac_04/list_exercises.py
@@ -9,3 +9,13 @@ print(f"The last number is {numbers[-1]}")
 print(f"The smallest number is {min(numbers)}")
 print(f"The largest number is {max(numbers)}")
 print(f"The average of numbers is {sum(numbers)/5}")
+
+usernames = ['jimbo', 'giltson98', 'derekf', 'WhatSup', 'NicolEye', 'swei45', 'BaseInterpreterInterface', 'BaseStdIn',
+             'Command', 'ExecState', 'InteractiveConsole', 'InterpreterInterface', 'StartServer', 'bob']
+
+try_username = input("Whats your username? ")
+
+if try_username in usernames:
+    print("Access Granted")
+else:
+    print("Access Denied")
