a2a54e6ce19d64c764e12a05ffbfebc84ae8db41
m2a <yewkheng.kwok@my.jcu.edu.au>
2023-12-03 12:12:27 +0800

    Modified Prac 4

--- prac_04/subject_reader.py
+++ prac_04/subject_reader.py
@@ -7,13 +7,14 @@ FILENAME = "subject_data.txt"
 
 
 def main():
+    """Formats the input data and prints in the specified format"""
     data = get_data()
     print(data)
     print_subjects(data)
 
 
 def get_data():
-    """Read data from file formatted like: subject,lecturer,number of students."""
+    """Read data from file formatted like: subject, lecturer, number of students."""
     input_file = open(FILENAME)
     subjects = []
 
